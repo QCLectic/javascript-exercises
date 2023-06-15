1fe0cb03fe73366fe84f74a54fc7da08ca3ed66a
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-15 16:29:54 -0500

    i had the right idea on the particular method but wasnt as educated on arrow functions. there was a different option to use but the filter option seemed to be better. also saw some modified files that were missed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,5 +3,6 @@ const reverseString = function(input) {
 };
 
 //take input string, split into individual characters, reverse order, print reverse.
+// i had started off with a reverse string with decrementing for loop before converting my code to reverse string just with built-in functions
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...theArgs) {
+    // let position=array.includes(...theArgs,0);
+    //return array.splice(position)
+    //   else return array;
+    // I had settled on using the array.filter but wasnt sure how to implement
+    //array.filter(function(...theArgs))
+    return array.filter(val => !theArgs.includes(val))
 };
-
+// using splice() to remove target in the array defined by ....theARGS
+// return array
 // Do not edit below this line
 module.exports = removeFromArray;
