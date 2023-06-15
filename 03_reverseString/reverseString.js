b8b83cc3899f0c12856a50347bf65d35a3dc2365
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-15 14:59:45 -0500

    freecodecamp to the rescue here. i had the logic correct. just needed the terminology.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    return input.split('').reverse().join('');
 };
 
+//take input string, split into individual characters, reverse order, print reverse.
 // Do not edit below this line
 module.exports = reverseString;
