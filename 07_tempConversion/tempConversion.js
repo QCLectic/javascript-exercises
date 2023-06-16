3b9a8dc241c0b3f2362c5d541e98911b5d650f29
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-16 15:23:01 -0500

    this was sweet revenge. flawless victory

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
 const convertToCelsius = function(f) {
-  c=(f-32)/1.8
+  c=(f-32)/1.8;
+  return cRound = Math.round(c*10)/10;
 };
 
 const convertToFahrenheit = function(c) {
-  f=(c*1.8)+32
-  return fRound = f;
+  f=(c*1.8)+32;
+  return fRound = Math.round(f*10)/10;
 };
 
 // Do not edit below this line
