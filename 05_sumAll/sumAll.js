f3486a650d76ea90ca3edd3d3f8b53345adb5238
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-16 12:05:23 -0500

    success after adding in a check for typeof

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(startNum,finNum) {
-    if (startNum < 0 || finNum < 0 || isNaN(startNum) || isNaN(finNum)) {
-        return 'Error';
+    if (typeof startNum !== 'number'|| typeof finNum !== 'number'||startNum < 0 || finNum < 0 || isNaN(startNum) || isNaN(finNum)) {
+        return 'ERROR';
       }
     
       const min = Math.min(startNum, finNum);
@@ -9,18 +9,5 @@ const sumAll = function(startNum,finNum) {
       const sum = ((max - min + 1) * (min + max)) / 2;
       return sum;
 };
-//okay so on this one we need to sum each number from start number to final number. 
-//startNum, FinNum
-//finNum-startNum to get difference AKA how often we need to iterate
-//iterate from startNum
-//i=diffNum; i<=; i++;
-// startNum
-//spit out error on non valid inputs
 
-//i think i need to actually have 2 parts. one to count and store each count to array
-// add the array
-// so we do need the start and fin.
-// Do not edit below this line
-
-// looks like we need to modify the logic to not accept negative numbers as well as to print error if a string is provided
 module.exports = sumAll;
