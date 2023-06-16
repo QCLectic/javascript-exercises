60a1e18087da3ca69b9f8394a960e3b50115a6ad
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-16 14:48:04 -0500

    leap year finished. i had logic correct but code needed refinement.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(startNum,finNum) {
     if (typeof startNum !== 'number'|| typeof finNum !== 'number'||startNum < 0 || finNum < 0 || isNaN(startNum) || isNaN(finNum)) {
         return 'ERROR';
-      }
+      }s
     
       const min = Math.min(startNum, finNum);
       const max = Math.max(startNum, finNum);
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //year is not divisable by 100 && is  divisable by 400 && is divsable by 4= leap year no
+    //else = leap year yes
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
 // Do not edit below this line
