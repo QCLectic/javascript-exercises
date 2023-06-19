c64f9c81a0a66bd19102ec006e20e520c7a70dfd
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-19 13:59:07 -0500

    completed 2 more modules. on fibonacci, I had to adjust values to start from 0 rather than 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 
 };
-
+palindromes('race car');
+//receive text as variable
+// remove spaces and punctuation
+// reverse text order as variable
+// compare text with rev text, if equal true, if not false.
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0, b = 1, temp;
+
+    if (num<0){
+        return 'OOPS'
+    } else
+        while (num >= 0){
+            temp = a;
+            a = a + b;
+            b = temp;
+            num--;
+        };
+        
+        return b;
+    ;
 
 };
+//loop up to number given
+//given number in sequence will equal previous function minus one combined with previous to last function minus 2
+//uses recursive function uses givent first term of sequence and pattern rule to get any term from previous term.
 
+// var num1 = 1, num2 = 0, temp;
+
+// while (num >= 0){
+//   temp = num1;
+//   num1 = num1 + num2;
+//   num2 = temp;
+//   num--;
+// }
+
+// return num2;
 // Do not edit below this line
 module.exports = fibonacci;
